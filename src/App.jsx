bc6543a36e64fbed0298f97e64d1fd8e85723e00
Gopal Kumar Das <krgopalkumar706@gmail.com>
2024-09-20 00:26:47 +0530

    basic- create msg,listMsg,delete msg function created

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ function App() {
   return (
     <>
       <Room/>
-      <h1>hello</h1>
       
     </>
   );
--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,25 +1,101 @@
 import { useEffect, useState } from "react";
+import { Trash2 } from "react-feather";
 import {
   databases,
   PROJECT_ID,
   DATABASE_ID,
   COLLECTION_ID_MESSAGES,
 } from "../appwriteConfig";
+import { ID, Query } from "appwrite";
 
 const Room = () => {
+  //useState Hooks
+  const [messages, setMessages] = useState([]);
+  const [messageBody, setMessageBody] = useState("");
+  // This function is called when the component mounts
   useEffect(() => {
     getMessages();
   }, []);
 
+  // This function is used for getting the messages from database using appwrite
   const getMessages = async () => {
     const response = await databases.listDocuments(
       DATABASE_ID,
-      COLLECTION_ID_MESSAGES
+      COLLECTION_ID_MESSAGES,
+      [Query.orderDesc("$createdAt"), Query.limit(20)]
     );
+
     console.log(response);
+    setMessages(response.documents);
   };
 
-  return <div>Room</div>;
+  //This function handles the chat submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const payload = {
+      body: messageBody,
+    };
+    let response = await databases.createDocument(
+      DATABASE_ID,
+      COLLECTION_ID_MESSAGES,
+      ID.unique(),
+      payload
+    );
+    console.log("Msg Created : ", response);
+    setMessages([...messages, response]);
+    setMessageBody("");
+  };
+  // This Function deletes the message
+  const deleteMessage = async (messageId) => {
+    const response = await databases.deleteDocument(
+      DATABASE_ID,
+      COLLECTION_ID_MESSAGES,
+      messageId
+    );
+    setMessages((prevMsg) =>
+      messages.filter((message) => message.$id !== messageId)
+    );
+  };
+
+  return (
+    <main className="container">
+      <div className="room--container">
+        <form onSubmit={handleSubmit} id="message--form" action="">
+          <div>
+            <textarea
+              required
+              maxLength={1000}
+              placeholder="Say Something..."
+              onChange={(e) => setMessageBody(e.target.value)}
+              value={messageBody}
+            ></textarea>
+          </div>
+          <div className="send-btn--wrapper">
+            <input className="btn btn--secondary" type="submit" value="Send" />
+          </div>
+        </form>
+        <div>
+          {messages.map((message) => (
+            <div key={message.$id} className="message--wrapper">
+              <div className="message--header">
+                <small className="message-timestamp">
+                  {new Date(message.$createdAt).toLocaleString()}
+                </small>
+
+                <Trash2 
+                className="delete--btn"
+                onClick={() => deleteMessage(message.$id)} />
+              </div>
+              <div className="message--body">
+                <span>{message.body}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Room;
