b69a3b18def5f915615d5889aed296c4d37cd6c0
Gopal Kumar Das <krgopalkumar706@gmail.com>
2024-11-30 09:03:06 +0530

    index js added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
-export { default as Room } from './Room'; 
-export { default as LoginPage } from './LoginPage'; 
-export { default as RegisterPage } from './RegisterPage'; 
+export { default as Room } from "./Room";
+export { default as LoginPage } from "./LoginPage";
+
+export { default as RegisterPage } from "./RegisterPage";
